23c2289917cc4f53f83710ad35136f694ab972e6
Tatjana Sikimic <tatjana.sikimic@rt-rk.com>
2026-05-11 12:02:53 +0200

    Update the name of the table

--- 3-terraform-dynamodb/variables.tf
+++ 3-terraform-dynamodb/variables.tf
@@ -23,5 +23,5 @@ variable "aws_endpoint" {
 variable "table_name" {
   description = "The name of the DynamoDB table"
   type        = string
-  default     = "g2-tatjana-table"
+  default     = "tatjana-table"
 }
