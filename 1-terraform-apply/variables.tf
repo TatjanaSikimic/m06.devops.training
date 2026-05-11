3d45d96a0d353f07c863601e1e357b573226a52c
Tatjana Sikimic <tatjana.sikimic@rt-rk.com>
2026-05-11 10:06:15 +0200

    Update bucket name

--- 1-terraform-apply/variables.tf
+++ 1-terraform-apply/variables.tf
@@ -23,6 +23,6 @@ variable "aws_endpoint" {
 variable "bucket_name" {
   description = "The name of the S3 bucket"
   type        = string
-  default     = "S3_g2_bucket_tatjana"
+  default     = "S3-g2-bucket-tatjana"
 }
 
